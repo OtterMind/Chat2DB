47b40fdc34b78a046bd833a7a0a35b8210a388d1
zgq <openai0229@gmail.com>
2026-07-24 23:21:11 +0800

    fix(postgresql): generate valid view comment SQL

--- chat2db-community-server/chat2db-community-plugins/chat2db-community-postgresql/src/test/java/ai/chat2db/plugin/postgresql/builder/PostgreSQLSqlBuilderTest.java
+++ chat2db-community-server/chat2db-community-plugins/chat2db-community-postgresql/src/test/java/ai/chat2db/plugin/postgresql/builder/PostgreSQLSqlBuilderTest.java
@@ -1,5 +1,6 @@
 package ai.chat2db.plugin.postgresql.builder;
 
+import ai.chat2db.community.domain.api.model.view.ModifyView;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +16,38 @@ class PostgreSQLSqlBuilderTest {
                 builder.appendSingleRowLimit("DELETE", "\"t\"", where, "DELETE FROM \"t\"" + where));
         assertEquals("UPDATE \"t\" set \"a\" = 1 where ctid in (select ctid from \"t\"" + where + " limit 1)",
                 builder.appendSingleRowLimit("UPDATE", "\"t\"", where, "UPDATE \"t\" set \"a\" = 1" + where));
+    }
+
+    @Test
+    void shouldQuoteQualifiedViewNameAndEscapeComment() {
+        PostgreSQLSqlBuilder builder = new PostgreSQLSqlBuilder();
+        ModifyView view = createView("report\"ing", "sales\"view", "owner's view");
+
+        assertEquals("CREATE VIEW \"report\"\"ing\".\"sales\"\"view\"\n"
+                        + "AS \n"
+                        + "SELECT 1 ;\n"
+                        + "comment on view \"report\"\"ing\".\"sales\"\"view\" is 'owner''s view';",
+                builder.buildCreateView(view));
+    }
+
+    @Test
+    void shouldOmitBlankSchemaFromCreateAndCommentViewNames() {
+        PostgreSQLSqlBuilder builder = new PostgreSQLSqlBuilder();
+        ModifyView view = createView("   ", "sales_view", "daily sales");
+
+        assertEquals("CREATE VIEW \"sales_view\"\n"
+                        + "AS \n"
+                        + "SELECT 1 ;\n"
+                        + "comment on view \"sales_view\" is 'daily sales';",
+                builder.buildCreateView(view));
+    }
+
+    private static ModifyView createView(String schemaName, String viewName, String comment) {
+        ModifyView view = new ModifyView();
+        view.setSchemaName(schemaName);
+        view.setViewName(viewName);
+        view.setViewBody("SELECT 1");
+        view.setComment(comment);
+        return view;
     }
 }
